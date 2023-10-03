e08e3a64f0ec22b0a5d5249b5db74eaa4c8e143d
anthony.enjalbert <anthony.enjalbert@iut-rodez.fr>
2023-10-03 17:27:39 +0200

    Initial commit

---
+++ src/main/java/but3/aenj/App.java
@@ -0,0 +1,14 @@
+package but3.aenj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
