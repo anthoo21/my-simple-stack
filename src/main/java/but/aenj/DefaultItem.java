ef64defdf71fe3da489f597436e1fe37ed5df941
anthony.enjalbert <anthony.enjalbert@iut-rodez.fr>
2023-10-10 14:25:40 +0200

    Modification fichier DefaultItem 2

--- src/main/java/but/aenj/DefaultItem.java
+++ src/main/java/but/aenj/DefaultItem.java
@@ -1,7 +1,7 @@
 package but.aenj;
 
 /**
- * Classe qui implémente l'interface Item. (modif pour projet)
+ * Classe qui implémente l'interface Item. (modif pour projet 2)
  */
 public class DefaultItem implements Item {
 
